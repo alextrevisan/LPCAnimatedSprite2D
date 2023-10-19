ccf3a3aea1517ea653df2edf1d25f7bd8728363a
Alexandro Trevisan <alexbaixista@gmail.com>
2023-10-18 23:20:54 -0300

    Added default loop for animation

---
+++ LPCAnimatedSprite/LPCSpriteData.gd
@@ -0,0 +1,11 @@
+class_name LPCAnimationData
+
+var FrameCount:int
+var Name:String
+var Row:int
+var Loop:bool
+func _init(frameCount:int, name:String, row:int, loop:bool):
+		FrameCount = frameCount
+		Name = name
+		Row = row
+		Loop = loop
