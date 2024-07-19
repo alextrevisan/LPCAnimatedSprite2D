486344e4abb08abfddec62a7fe2d1b7ce6742639
Alexandro Trevisan <alexbaixista@gmail.com>
2024-07-18 23:13:10 -0300

    Update for Sprite3D support

--- addons/LPCAnimatedSprite/LPCAnimatedSprite2D.gd
+++ addons/LPCAnimatedSprite/LPCAnimatedSprite2D.gd
@@ -1,10 +1,22 @@
 @tool
-extends Node2D
+extends Node
 
 class_name LPCAnimatedSprite2D
 
 @export var SpriteSheets:Array[LPCSpriteSheet]
 @export var DefaultAnimation:LPCEnum.LPCAnimation = LPCEnum.LPCAnimation.IDLE_DOWN
+@export var NodeType:LPCEnum.ESpriteNodeType = LPCEnum.ESpriteNodeType.Sprite_2D
+
+@export_group("2D Properties")
+@export var Sprite2DTextureFilter:CanvasItem.TextureFilter = CanvasItem.TEXTURE_FILTER_NEAREST
+
+@export_group("3D Properties")
+const DEFAULT_3D_PIXEL_SIZE:float = 0.01
+@export var Sprite3DScale:float = 1
+@export var Sprite3DBillboard:BaseMaterial3D.BillboardMode = BaseMaterial3D.BILLBOARD_DISABLED
+@export var Sprite3DTextureFilter:BaseMaterial3D.TextureFilter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
+
+var lastOffset:float = 1.0
 
 var AnimationNames:Array
 func _ready():
@@ -39,8 +51,8 @@ func LoadAnimations():
 		if spriteSheet == null:
 			push_warning("There are LPCSpriteSheets that are <empty> in the LPCAnimatedSprite2D panel")
 			continue
-		var animatedSprite = AnimatedSprite2D.new()
-		animatedSprite.texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+			
+		var animatedSprite = CreateSpriteType()
 		var spriteFrames = CreateSprites(spriteSheet)
 		animatedSprite.frames = spriteFrames
 		add_child(animatedSprite)
@@ -50,7 +62,21 @@ func LoadAnimations():
 			animatedSprite.name = spriteSheet.Name
 		animatedSprite.owner = get_tree().edited_scene_root
 		play(DefaultAnimation)
-
+func CreateSpriteType():
+	match NodeType:
+		LPCEnum.ESpriteNodeType.Sprite_3D:
+			var animatedSprite = AnimatedSprite3D.new()
+			animatedSprite.pixel_size = Sprite3DScale * DEFAULT_3D_PIXEL_SIZE
+			animatedSprite.texture_filter = Sprite3DTextureFilter
+			animatedSprite.billboard = Sprite3DBillboard
+			animatedSprite.sorting_offset = lastOffset
+			lastOffset += 1.0
+			return animatedSprite
+		LPCEnum.ESpriteNodeType.Sprite_2D:
+			var animatedSprite = AnimatedSprite2D.new()
+			animatedSprite.texture_filter = Sprite2DTextureFilter
+			return animatedSprite
+			
 func CreateSprites(spriteSheet:LPCSpriteSheet):
 	var spriteFrames = SpriteFrames.new()
 	spriteFrames.remove_animation("default")
