4134de689a2a90af22aa796ecb89ffe8550c4646
Alexandro Trevisan <alexbaixista@gmail.com>
2025-02-21 22:06:42 -0300

    removed comments

--- addons/LPCAnimatedSprite/LPCAnimatedSprite.gd
+++ addons/LPCAnimatedSprite/LPCAnimatedSprite.gd
@@ -2,9 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	# Initialization of the plugin goes here
 	add_custom_type("LPCAnimatedSprite", "AnimatedSprite2D", preload("LPCAnimatedSprite2D.gd"), preload("icon2d.png"))
 
 func _exit_tree():
-	# Clean-up of the plugin goes here
 	remove_custom_type("LPCAnimatedSprite")
