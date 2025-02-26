8233e46c269f942ed29bef3334cd76a71a64931d
Alexandro Trevisan <alexbaixista@gmail.com>
2025-02-26 15:50:53 -0300

    updated assets to the old example

--- example_assets/example.gd
+++ example_assets/example.gd
@@ -1,7 +1,10 @@
+@tool
 extends Node2D
 
-#precompile spritesheet definition
-#https://sanderfrenken.github.io/Universal-LPC-Spritesheet-Character-Generator/#?body=Body_color_light&head=Human_male_light&weapon=Dagger_dagger&sex=male&shadow=Shadow_shadow&wound_arm=none&prosthesis_hand=none&shoulders=Plate_steel&arms=Armour_steel&bauldron=Bauldron_tan&bracers=Bracers_steel&wrists=none&gloves=none&armour=Plate_steel&cape=Tattered_purple&belt=Leather_Belt_brown&legs=Armour_steel&shoes=Armour_steel&eyepatch=Eyepatch_eyepatch&hair=Plain_ash
+#LPC spritesheet definition
+#https://liberatedpixelcup.github.io/Universal-LPC-Spritesheet-Character-Generator/#?body=Body_color_light&head=Human_male_light&weapon=Longsword_longsword&sex=male&shadow=Shadow_shadow&wound_arm=none&prosthesis_hand=none&shoulders=Plate_steel&arms=Armour_steel&bauldron=Bauldron_tan&bracers=Bracers_steel&wrists=none&gloves=none&armour=Plate_steel&cape=Tattered_purple&belt=Leather_Belt_brown&legs=Armour_steel&shoes=Armour_steel&eyepatch=Eyepatch_eyepatch&hair=Plain_ash
+#Sprout Lands
+#https://cupnooble.itch.io/sprout-lands-asset-pack
 
 @onready var multiple: LPCAnimatedSprite2D = $CanvasLayer/GridContainer/Control/LPCAnimatedSprite2D
 @onready var single: LPCAnimatedSprite2D = $CanvasLayer/GridContainer/Control2/LPCAnimatedSprite2D
@@ -17,8 +20,6 @@ func _ready() -> void:
 	_load_data_from_first_animation()
 	for index in range(3):
 		animations[index].play(animation_select[index].get_item_text(0))
-	
-	
 
 func _load_data_from_first_animation() -> void:
 	for i in range(3):
@@ -26,7 +27,6 @@ func _load_data_from_first_animation() -> void:
 			for dir:String in animations[i].animation_data.available_directions[anim]:
 				var anim_dir:String = anim + "_" + dir
 				animation_select[i].add_item(anim_dir)
-
 
 func _on_option_button_item_selected(index: int, extra_arg_0: int) -> void:
 	var anim:String = animation_select[extra_arg_0].get_item_text(index)
